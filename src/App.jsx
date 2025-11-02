237a7cf614d41369a7ddb2d21205bdd9c56448b5
brundha <brundha2001@gmail.com>
2025-11-03 03:43:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="src/assets/react.svg" alt="react logo" />
+      </header>
+      <h1>Fun facts about React</h1>
+      <ol>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ol>
+      <footer>
+        <small>© 2025 Makam's development. All rights reserved.</small>
+      </footer>
+    </main>
+  );
+}
+
+export default App
