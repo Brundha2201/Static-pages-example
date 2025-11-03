be6836d86aa29a03ce24199f7a3d1396df8aee41
brundha <brundha2001@gmail.com>
2025-11-04 02:46:39 +0530

    Project-1: Creating  static pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react.svg" alt="react logo" />
-    </header>
-  )
-}
-
-function MainContent() {
-  return (
-    <main>
-      <h1>Fun facts about React</h1>
-      <ol>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
-    </main>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2025 Makam's development. All rights reserved.</small>
-    </footer>
-  )
-}
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header className="header">
+            <nav>
+                <img src="src/assets/react.svg" className="nav-logo" alt="react logo" />
+                <span className="nav-text">React Facts</span>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
